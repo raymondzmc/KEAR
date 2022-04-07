0d26ba8c8cf1e66bfd7f158064dcfff12d2cde03
Raymond Li <li.raymond96@gmail.com>
2022-04-06 17:35:10 -0700

    fixed major bug

--- bash/compute_canada_interactions/compute_canada_interactions_deberta_8.sh
+++ bash/compute_canada_interactions/compute_canada_interactions_deberta_8.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:v100l:1      # Request GPU "generic resources"
 #SBATCH --cpus-per-task=3  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=1-0:00
+#SBATCH --time=0-12:00
 #SBATCH --output=interactions_debertav3-large_8.out
 
 module load python
